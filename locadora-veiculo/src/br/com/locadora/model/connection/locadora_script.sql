568f147279b90294fb7399b3d3daffa78addf403
Joaquim Neto <jsn.piata@gmail.com>
2014-10-05 20:33:33 -0300

    Atualização do script da base de dados locadora

--- locadora-veiculo/src/br/com/locadora/model/connection/locadora_script.sql
+++ locadora-veiculo/src/br/com/locadora/model/connection/locadora_script.sql
@@ -7,16 +7,20 @@ CREATE TABLE agencia (
  razao_social VARCHAR(200) NOT NULL,
  fantasia VARCHAR(150) NOT NULL,
  cnpj VARCHAR(21) NOT NULL,
- insc_estadual VARCHAR(45) NOT NULL,
+ insc_estadual VARCHAR(12) NOT NULL,
+ ins_municipal VARCHAR(100),
  logradouro VARCHAR(50) NOT NULL,
  bairro VARCHAR(25) NOT NULL,
  numero VARCHAR(7) NOT NULL,
- cep VARCHAR(9) NOT NULL,
+ cep VARCHAR(8) NOT NULL,
  cidade VARCHAR(30) NOT NULL,
  uf VARCHAR(2) NOT NULL,
  telefone VARCHAR(11),
  email VARCHAR(45),
- site VARCHAR(100)
+ site VARCHAR(100),
+ data_cadastro DATETIME,
+ data_manutencao DATETIME,
+ ativo INT
 );
 
 
@@ -29,16 +33,20 @@ CREATE TABLE cliente (
  cnh VARCHAR(11) NOT NULL,
  validade_cnh DATE NOT NULL,
  estado_emissor VARCHAR(45) NOT NULL,
- genero INT,
+ genero CHAR(1),
  logradouro VARCHAR(50) NOT NULL,
  bairro VARCHAR(30),
- numero VARCHAR(7) NOT NULL,
+ numero INT NOT NULL,
  cep VARCHAR(8) NOT NULL,
  cidade VARCHAR(25) NOT NULL,
  uf VARCHAR(2) NOT NULL,
  telefone VARCHAR(11),
  email VARCHAR(45),
  id_agencia INT(11),
+ id_funcionario INT(11) NOT NULL,
+ data_cadastro DATETIME,
+ data_manutencao DATETIME,
+ ativo INT,
 
  FOREIGN KEY (id_agencia) REFERENCES agencia (id_agencia)
 );
@@ -52,7 +60,7 @@ CREATE TABLE funcionario (
  rg VARCHAR(9) NOT NULL,
  genero CHAR(1) NOT NULL,
  logradouro VARCHAR(50) NOT NULL,
- numero VARCHAR(7) NOT NULL,
+ numero INT NOT NULL,
  bairro VARCHAR(30),
  cep VARCHAR(8) NOT NULL,
  cidade VARCHAR(25) NOT NULL,
@@ -65,18 +73,17 @@ CREATE TABLE funcionario (
  data_manutencao DATETIME,
  funcionario_supervisor INT(11),
  id_agencia INT(11),
+ ativo INT NOT NULL,
 
  FOREIGN KEY (funcionario_supervisor) REFERENCES funcionario (id_funcionario),
- FOREIGN KEY (id_agencia) REFERENCES agencia (id_agencia),
-
- CONSTRAINT USUARIO_UNIQUE UNIQUE(usuario)
+ FOREIGN KEY (id_agencia) REFERENCES agencia (id_agencia)
 );
 
 
 CREATE TABLE pagamento (
  id_pagamento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
- tipo VARCHAR(30),
- nome VARCHAR(60),
+ tipo VARCHAR(50),
+ nome VARCHAR(100),
  cpf VARCHAR(11),
  data_pagamento DATE
 );
@@ -92,13 +99,18 @@ CREATE TABLE veiculo (
  acessorio VARCHAR(350),
  chassi VARCHAR(17),
  placa VARCHAR(7),
- cidade VARCHAR(25),
+ cidade VARCHAR(150),
  uf VARCHAR(2),
  kmrodado FLOAT,
  precokmlivre FLOAT,
  precokmcontrolado FLOAT,
  status_veiculo VARCHAR(45),
  id_agencia INT(11),
+ id_funcionario INT(11) NOT NULL,
+ status INT(11),
+ data_cadastro DATETIME,
+ data_manutencao DATETIME,
+ ativo INT,
 
  FOREIGN KEY (id_agencia) REFERENCES agencia (id_agencia)
 );
@@ -116,7 +128,10 @@ CREATE TABLE locacao (
  tipo_pagamento CHAR(10),
  valor_acrescimo FLOAT,
  id_veiculo INT(11),
- id_pagamento INT,
+ id_pagamento INT NOT NULL,
+ id_funcionario INT NOT NULL,
+ id_agencia INT NOT NULL,
+ status INT,
 
  FOREIGN KEY (id_veiculo) REFERENCES veiculo (id_veiculo),
  FOREIGN KEY (id_pagamento) REFERENCES pagamento (id_pagamento)
@@ -133,4 +148,3 @@ CREATE TABLE cliente_locacao (
  FOREIGN KEY (id_cliente) REFERENCES cliente (id_cliente),
  FOREIGN KEY (id_locacao) REFERENCES locacao (id_locacao)
 );
-
