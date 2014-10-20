4a4c6afbd8665ba54308ad0dd4cd772991af855c
Bruno Geronimo <contato@brunogeronimo.com.br>
2014-10-20 00:57:22 -0200

    Alteração DAO de locação

--- locadora-veiculo/src/br/com/locadora/model/DAO/LocacaoDAO.java
+++ locadora-veiculo/src/br/com/locadora/model/DAO/LocacaoDAO.java
@@ -2,8 +2,8 @@ package br.com.locadora.model.DAO;
 import br.com.locadora.model.entity.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Date;
 import java.util.List;
-import java.util.Date;
 
 import br.com.locadora.model.connection.MysqlConnect;
 
@@ -21,14 +21,29 @@ public class LocacaoDAO extends MysqlConnect{
 			if (resultSet.next()){				
 				resultado.setId(resultSet.getInt(1));
 				resultado.setDataHoraLocacao(resultSet.getDate(2));
-				resultado.setDataHoraDevolucao(resultSet.getDate(3));
-				resultado.setAgenciaDevolucao(resultSet.getInt(4));
-				resultado.setTipoTarifa(resultSet.getInt(5));
-				resultado.setKmLocacao(resultSet.getDouble(6));
-				resultado.setKmDevolucao(resultSet.getDouble(7));
-				resultado.setValor(resultSet.getDouble(8));
-				resultado.setValorAcrescimo(resultSet.getDouble(10));
-				
+				resultado.setDataHoraPrevistaDevolucao(resultSet.getDate(3));
+				resultado.setDataHoraDevolucao(resultSet.getDate(4));
+				resultado.setAgenciaDevolucao(resultSet.getInt(5));
+				resultado.setTipoTarifa(resultSet.getInt(6));
+				resultado.setKmLocacao(resultSet.getDouble(7));
+				resultado.setKmDevolucao(resultSet.getDouble(8));
+				resultado.setValor(resultSet.getDouble(9));
+				resultado.setTipoPagamento(resultSet.getString(10));
+				resultado.setValorAcrescimo(resultSet.getDouble(11));
+				resultado.setStatus(resultSet.getInt(12));
+				Veiculo veiculo = new Veiculo();
+				veiculo.setId(resultSet.getInt(13));
+				resultado.setIdVeiculo(veiculo);
+				Cliente cliente = new Cliente();
+				cliente.setId(resultSet.getInt(14));
+				resultado.setCliente(cliente);
+				Pagamento pagamento = new Pagamento();
+				pagamento.setId(resultSet.getInt(15));
+				resultado.setPagamento(pagamento);
+				resultado.setIdFuncionario(resultSet.getInt(16));
+				Agencia agencia = new Agencia();
+				agencia.setIdAgencia(resultSet.getInt(17));
+				resultado.setIdAgencia(agencia.getIdAgencia());				
 			}
 			return resultado;
 		}catch(Exception selectError){
@@ -39,34 +54,43 @@ public class LocacaoDAO extends MysqlConnect{
 	public boolean update(Locacao locacao) {
 		PreparedStatement sqlSt;
 		try{
-			String sql = "UPDATE locacao" + 
-							"SET" + 
-								"data_hora_locacao = ?," + 
-								"data_hora_prevista_devolucao = ?," + 
-								"agencia_devolucao = ?," + 
-								"tipo_tarifa = ?," + 
-								"km_locacao = ?," + 
-								"km_devolucao = ?," + 
-								"valor = ?," + 
-								"tipo_pagamento = ?," + 
-								"valor_acrescimo = ?," + 
-								"id_veiculo = ?," + 
-								"id_pagamento = ?" + 
-							"WHERE id_locacao = ?";
+			String sql = "UPDATE locacao" +
+						"SET" +
+							"data_hora_locacao = ?," +
+							"data_hora_prevista_devolucao = ?," +
+							"data_hora_devolucao = ?," +
+							"agencia_devolucao = ?," +
+							"tipo_tarifa = ?," +
+							"km_locacao = ?," +
+							"km_devolucao = ?," +
+							"valor = ?," +
+							"tipo_pagamento = ?," +
+							"valor_acrescimo = ?," +
+							"status = ?," +
+							"id_veiculo = ?," +
+							"id_cliente = ?," +
+							"id_pagamento = ?," +
+							"id_funcionario = ?," +
+							"id_agencia = ?" +
+						"WHERE id_locacao = ?";
 			sqlSt = conn.prepareStatement(sql);
-			sqlSt.setDate(1, new java.sql.Date(locacao.getDataHoraLocacao().getTime()));
-			sqlSt.setDate(2,  new java.sql.Date(locacao.getDataHoraDevolucao().getTime()));
-			sqlSt.setInt(3, locacao.getAgenciaDevolucao());
-			sqlSt.setInt(4, locacao.getTipoTarifa());
-			sqlSt.setDouble(5, locacao.getKmLocacao());
-			sqlSt.setDouble(6, locacao.getKmDevolucao());
-			sqlSt.setDouble(7, locacao.getValor());
-			sqlSt.setDouble(8, locacao.getTipoTarifa());
+			sqlSt.setDate(1, new  java.sql.Date(locacao.getDataHoraLocacao().getTime()));
+			sqlSt.setDate(2, new java.sql.Date(locacao.getDataHoraPrevistaDevolucao().getTime()));
+			sqlSt.setDate(3, new java.sql.Date(locacao.getDataHoraDevolucao().getTime()));
+			sqlSt.setInt(4, locacao.getAgenciaDevolucao());
+			sqlSt.setInt(5, locacao.getTipoTarifa());
+			sqlSt.setDouble(6, locacao.getKmLocacao());
+			sqlSt.setDouble(7, locacao.getKmDevolucao());
+			sqlSt.setDouble(8, locacao.getValor());
 			sqlSt.setString(9, locacao.getTipoPagamento());
 			sqlSt.setDouble(10, locacao.getValorAcrescimo());
-			sqlSt.setInt(11, locacao.getVeiculo().getId());
-			sqlSt.setInt(12, locacao.getPagamento().getId());
-			sqlSt.setInt(13, locacao.getId());
+			sqlSt.setInt(11, locacao.getStatus());
+			sqlSt.setInt(12, locacao.getVeiculo().getId());
+			sqlSt.setInt(13, locacao.getCliente().getId());
+			sqlSt.setInt(14, locacao.getPagamento().getId());
+			sqlSt.setInt(15, locacao.getIdFuncionario());
+			sqlSt.setInt(16, locacao.getIdAgencia());
+			sqlSt.setInt(17, locacao.getId());
 			sqlSt.executeQuery();
 			return true;
 		}catch(Exception updateError){
@@ -80,6 +104,7 @@ public class LocacaoDAO extends MysqlConnect{
 			String sql = "INSERT INTO locacao" +
 							"(data_hora_locacao," +
 							"data_hora_prevista_devolucao," +
+							"data_hora_devolucao," +
 							"agencia_devolucao," +
 							"tipo_tarifa," +
 							"km_locacao," +
@@ -87,23 +112,31 @@ public class LocacaoDAO extends MysqlConnect{
 							"valor," +
 							"tipo_pagamento," +
 							"valor_acrescimo," +
+							"status," +
 							"id_veiculo," +
-							"id_pagamento)" +
+							"id_cliente," +
+							"id_pagamento," +
+							"id_funcionario," +
+							"id_agencia)" +
 						"VALUES" +
-							"(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?";
+							"(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			sqlSt = conn.prepareStatement(sql);
-			sqlSt.setDate(1, new java.sql.Date(locacao.getDataHoraLocacao().getTime()));
-			sqlSt.setDate(2,  new java.sql.Date(locacao.getDataHoraDevolucao().getTime()));
-			sqlSt.setInt(3, locacao.getAgenciaDevolucao());
-			sqlSt.setInt(4, locacao.getTipoTarifa());
-			sqlSt.setDouble(5, locacao.getKmLocacao());
-			sqlSt.setDouble(6, locacao.getKmDevolucao());
-			sqlSt.setDouble(7, locacao.getValor());
-			sqlSt.setDouble(8, locacao.getTipoTarifa());
+			sqlSt.setDate(1, new  java.sql.Date(locacao.getDataHoraLocacao().getTime()));
+			sqlSt.setDate(2, new java.sql.Date(locacao.getDataHoraPrevistaDevolucao().getTime()));
+			sqlSt.setDate(3, new java.sql.Date(locacao.getDataHoraDevolucao().getTime()));
+			sqlSt.setInt(4, locacao.getAgenciaDevolucao());
+			sqlSt.setInt(5, locacao.getTipoTarifa());
+			sqlSt.setDouble(6, locacao.getKmLocacao());
+			sqlSt.setDouble(7, locacao.getKmDevolucao());
+			sqlSt.setDouble(8, locacao.getValor());
 			sqlSt.setString(9, locacao.getTipoPagamento());
 			sqlSt.setDouble(10, locacao.getValorAcrescimo());
-			sqlSt.setInt(11, locacao.getVeiculo().getId());
-			sqlSt.setInt(12, locacao.getPagamento().getId());
+			sqlSt.setInt(11, locacao.getStatus());
+			sqlSt.setInt(12, locacao.getVeiculo().getId());
+			sqlSt.setInt(13, locacao.getCliente().getId());
+			sqlSt.setInt(14, locacao.getPagamento().getId());
+			sqlSt.setInt(15, locacao.getIdFuncionario());
+			sqlSt.setInt(16, locacao.getIdAgencia());
 			sqlSt.executeQuery();
 			return true;
 		}catch(Exception updateError){
